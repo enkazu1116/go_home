f6f9302aa16583375d01b2a70da76d6fabd85551
endoKazuki <enkazu1116@gmail.com>
2025-07-30 23:45:11 +0900

    即帰宅API作成最初のコミット

---
+++ internal/entity/attendance.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"time"
+)
+
+type Attendance struct {
+	ID        string    `gorm:"primaryKey"`
+	UserID    string    `gorm:"primaryKey"`
+	Date      time.Time `gorm:"primaryKey"`
+	CheckIn   time.Time
+	CheckOut  time.Time
+	IsLate    bool
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
---
+++ internal/entity/user.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        string         `gorm:"primaryKey"`
+	AuthID    string         `gorm:"unique;not null"`
+	Name      string         `gorm:"not null"`
+	Email     string         `gorm:"unique;not null"`
+	Role      string         `gorm:"not null"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+}
